dae4d86face69440df62fdb5aa2ea0b1d4d14f18
Bob Moore <@remoorejr>
2016-11-10 18:21:44 -0500

    refactor code

--- src/ios/PasscodeCheck.m
+++ src/ios/PasscodeCheck.m
@@ -19,34 +19,42 @@
     {
         LAContext *laContext = [[LAContext alloc] init];
         NSError *passcodeError = nil;
+	
 
-        if ([laContext().canEvaluatePolicy(.DeviceOwnerAuthentication, error: &passcodeError]) 
-        {   
-            [laContext().canEvaluatePolicy(.DeviceOwnerAuthentication, error: &passcodeError:^(BOOL success, NSError *error)
-            if (success) {
-                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-            } else {
-                NSString *errorCode =nil;
-                switch (error.code) { 
-                         case LAErrorSystemCancel:
-                             errorCode = @"systemCancel";
-                             break;
-                         case LAErrorPasscodeNotSet:
-                             errorCode = @"passcodeNotSet";
-                             break;
-                         default:
-                             errorCode = @"unknown";
-                             break;
-                }
-                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorCode];
-            }    
-        ]};
-    }
-     else
-    {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        if ([laContext canEvaluatePolicy: LAPolicyDeviceOwnerAuthentication error: &passcodeError])
+		
+        {
+			[laContext evaluatePolicy:LAPolicyDeviceOwnerAuthentication localizedReason: text reply: ^(BOOL success, NSError *error)
+		
+			{
+				if (success) {
+					pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+				} else {
+					NSString *errorCode =nil;
+					switch (error.code) {
+						case LAErrorSystemCancel:
+							errorCode = @"systemCancel";
+							break;
+						case LAErrorPasscodeNotSet:
+							errorCode = @"passcodeNotSet";
+							break;
+						default:
+							errorCode = @"unknown";
+							break;
+					}
+					pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorCode];
+				}
+				[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+			}];
+		}
+		else
+		{
+			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+		}
+	}
+	
+	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
 }
 
 @end
