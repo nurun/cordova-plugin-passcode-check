3f275ff1f38fddc1131a360354d7af488c9189fe
Bob Moore <@remoorejr>
2016-11-11 10:59:24 -0500

    refactor code

--- www/passcodeCheck.js
+++ www/passcodeCheck.js
@@ -5,8 +5,11 @@ var PasscodeCheck = function () {
     this.name = "PasscodeCheck";
 };
 
-PasscodeCheck.prototype.devicePasscodeIsSet = function (successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "PasscodeCheck", "devicePasscodeIsSet");
+PasscodeCheck.prototype.devicePasscodeIsSet = function (successCallback, errorCallback, text) {
+	 if (!text) {
+        text = "Verifying device passcode exists.";
+    }
+    exec(successCallback, errorCallback, "PasscodeCheck", "devicePasscodeIsSet", [text]);
 };
 
 
