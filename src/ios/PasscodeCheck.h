a503170f94b125c35efe29cc3a0a0462f819371e
Bob Moore <@remoorejr>
2016-11-11 16:19:04 -0500

    Revised copyright notices

--- src/ios/PasscodeCheck.h
+++ src/ios/PasscodeCheck.h
@@ -1,6 +1,8 @@
 //
 //  PasscodeCheck.h
-//  Copyright (c) 2016 R.E. Moore Jr. - @remoorejr
+//  Copyright (c) 2016 R.E. Moore Jr. & Alpha Software, Inc.
+//  Twitter: @remoorejr
+//  www.alphasoftware.com
 //
 
 #import <Cordova/CDVPlugin.h>
